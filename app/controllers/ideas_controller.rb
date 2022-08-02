c253586e87d2985a8e88bc9270cfefca1da18d84
mikecoolio <mikepoon604@gmail.com>
2022-08-02 15:27:39 -0700

    created delete feature

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,10 +1,12 @@
 class IdeasController < ApplicationController
+    before_action :find_idea, only: [:show, :edit, :update, :destroy]
+
     def new
        @idea = Idea.new 
     end
 
     def create
-        @idea = Idea.new(params.require(:idea).permit(:title, :body))
+        @idea = Idea.new(idea_params)
 
         if @idea.save
             flash[:notice] = "Successfully created idea"
@@ -17,7 +19,6 @@ class IdeasController < ApplicationController
     end
 
     def show
-        @idea = Idea.find(params[:id])
     end
 
     def edit
@@ -25,10 +26,30 @@ class IdeasController < ApplicationController
     end
 
     def update
-
+        if @idea.update(idea_params)
+            flash[:notice] = "Successfully updated idea"
+            redirect_to idea_path(@idea)
+        else
+            render :edit
+        end
     end
 
     def destroy
-        
+        if @idea.present?
+            @idea.destroy
+        else
+            flash[:alert] = "Failed to destroy this idea"
+        end
+        redirect_to ideas_path
+    end
+
+    private
+
+    def find_idea
+        @idea = Idea.find(params[:id])
+    end
+
+    def idea_params
+        params.require(:idea).permit(:title, :body)
     end
 end
