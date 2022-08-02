62ca2dc9c1d8bccb7f35f50df69c2942310b3ba7
mikecoolio <mikepoon604@gmail.com>
2022-08-02 10:59:50 -0700

    created show and index page

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -5,10 +5,19 @@ class IdeasController < ApplicationController
 
     def create
         @idea = Idea.new(params.require(:idea).permit(:title, :body))
+
+        if @idea.save
+            flash[:notice] = "Successfully created idea"
+            redirect_to idea_path(@idea)
+        end
     end
 
     def index
         @ideas = Idea.order(created_at: :desc)
+    end
+
+    def show
+        @idea = Idea.find(params[:id])
     end
 
     def edit
